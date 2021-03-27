5dfd932e515fdc0d31bc88823518d940a76d7ed7
robert <robert.dumitrescu.dev@gmail.com>
2021-03-27 15:35:28 +0200

    improve procesing algoritm

--- src/main/java/com/dejava/shipDestroyer/service/ProcessingService.java
+++ src/main/java/com/dejava/shipDestroyer/service/ProcessingService.java
@@ -1,5 +1,6 @@
 package com.dejava.shipDestroyer.service;
 
+import com.dejava.shipDestroyer.model.BattleshipCell;
 import com.dejava.shipDestroyer.model.GameStartedEvent;
 import org.springframework.stereotype.Service;
 
@@ -7,20 +8,26 @@ import java.util.*;
 
 @Service
 public class ProcessingService {
-  public void onHitEvent(int boardX, int boardY) {
-    GameStartedEvent game = new GameStartedEvent();
-    int battlegroundSize = game.getBattlegroundSize();
+  private boolean checkCanvas() {
+    return true;
+  }
 
-    String[][] battleground = new String[battlegroundSize][battlegroundSize];
-    HashMap<String, Integer> possibleOptions = new HashMap<String, Integer>();
+  private HashMap<String, Integer> possibleOptions = new HashMap<String, Integer>();
 
-    battleground[4][6] = "hit";
-    battleground[5][5] = "miss";
+  private BattleshipCell[][] rotateClockWise(BattleshipCell[][] canvas) {
+    int n = canvas.length;
+    int m = canvas[0].length;
+    BattleshipCell [][] output = new BattleshipCell [m][n];
 
-    var battleshipTemplate = game.getBattleshipTemplate();
-    var canvas = battleshipTemplate.getCanvasList();
-    int width = battleshipTemplate.getWidth();
-    int height = battleshipTemplate.getHeight();
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        output [j][n - 1 - i] = canvas[i][j];
+    return output;
+  }
+
+  private void addOptions(BattleshipCell[][] canvas, int boardY, int boardX, int battlegroundSize, String[][] battleground) {
+    int height = canvas.length;
+    int width = canvas[0].length;
 
     for (int templateY = 0; templateY < height; templateY++) {
       for (int templateX = 0; templateX < width; templateX++) {
@@ -29,11 +36,12 @@ public class ProcessingService {
           for (int canvasY = 0; canvasY < height; canvasY++) {
             if (!ok) break;
             for (int canvasX = 0; canvasX < width; canvasX++) {
-              if (!ok) break;
-              if (boardY - templateY + canvasY < 0 || boardY - templateY + canvasY > battlegroundSize) { ok = false; break; }
-              if (boardX - templateX + canvasX < 0 || boardX - templateX + canvasX > battlegroundSize) { ok = false; break; }
-              if (battleground[boardY - templateY + canvasY][boardX - templateX + canvasX] == "hit" && canvas[canvasY][canvasX].getHp() == 0) { ok = false; break; }
-              if (battleground[boardY - templateY + canvasY][boardX - templateX + canvasX] == "miss" && canvas[canvasY][canvasX].getHp() != 0) { ok = false; break; }
+              int newY = boardY - templateY + canvasY;
+              int newX = boardX - templateX + canvasX;
+              if (newY < 0 || newY > battlegroundSize) { ok = false; break; }
+              if (newX < 0 || newX > battlegroundSize) { ok = false; break; }
+              if (battleground[newY][newX] == "hit" && canvas[canvasY][canvasX].getHp() == 0) { ok = false; break; }
+              if (battleground[newY][newX] == "miss" && canvas[canvasY][canvasX].getHp() != 0) { ok = false; break; }
             }
           }
           if (ok) {
@@ -55,6 +63,23 @@ public class ProcessingService {
         }
       }
     }
+  }
+
+  public void onHitEvent(int boardX, int boardY) {
+    GameStartedEvent game = new GameStartedEvent();
+    int battlegroundSize = game.getBattlegroundSize();
+
+    String[][] battleground = new String[battlegroundSize][battlegroundSize];
+
+    var battleshipTemplate = game.getBattleshipTemplate();
+    var canvasNorth = battleshipTemplate.getCanvasList();
+    this.addOptions(canvasNorth, boardY,boardX,battlegroundSize,battleground);
+    var canvasEast = rotateClockWise(canvasNorth);
+    this.addOptions(canvasEast, boardY,boardX,battlegroundSize,battleground);
+    var canvasSouth = rotateClockWise(canvasEast);
+    this.addOptions(canvasSouth, boardY,boardX,battlegroundSize,battleground);
+    var canvasWest = rotateClockWise(canvasSouth);
+    this.addOptions(canvasWest, boardY,boardX,battlegroundSize,battleground);
 
      String maxKey = Collections.max(possibleOptions.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
      String[] arrOfStr =maxKey.split("-");
