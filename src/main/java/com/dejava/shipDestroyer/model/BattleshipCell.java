399b3553cd1267923c9dae9bb0d784ceeb276f50
Fulber <ovidiu.negoita95@gmail.com>
2021-03-27 12:45:12 +0200

    kafka

--- src/main/java/com/dejava/shipDestroyer/model/BattleshipCell.java
+++ src/main/java/com/dejava/shipDestroyer/model/BattleshipCell.java
@@ -2,6 +2,30 @@ package com.dejava.shipDestroyer.model;
 
 public class BattleshipCell {
 
-    private Integer hp;
+    private int hp;
     private String symbol;
+
+    public BattleshipCell() {
+    }
+
+    public BattleshipCell(int hp, String symbol) {
+        this.hp = hp;
+        this.symbol = symbol;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
 }
