4ce47a63bb1a61bc32cb725de69a46e8d5546757
Fulber <ovidiu.negoita95@gmail.com>
2021-03-27 12:34:54 +0200

    fixed topic name

--- src/main/java/com/dejava/shipDestroyer/service/Consumer.java
+++ src/main/java/com/dejava/shipDestroyer/service/Consumer.java
@@ -1,6 +1,8 @@
 package com.dejava.shipDestroyer.service;
 
 import com.dejava.shipDestroyer.model.GameStartedEvent;
+import com.dejava.shipDestroyer.model.RoundEndedEvent;
+import com.dejava.shipDestroyer.model.RoundStartedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +26,21 @@ public class Consumer {
     @Value(value = "${tournamentId}")
     private String tournamentId;
 
-    @KafkaListener(topics = " cc.battleships.game.started")
+    @KafkaListener(topics = "cc.battleships.game.started")
     public void gameStartedEvent(GameStartedEvent event) throws IOException {
         logger.info(String.format("#### -> Consumed message -> %s", event));
 
+    }
+
+    @KafkaListener(topics = "cc.battleships.round.started")
+    public void roundStartedEvent(RoundStartedEvent event) throws IOException {
+        logger.info(String.format("#### -> Consumed message -> %s", event));
+
+    }
+
+    @KafkaListener(topics = "cc.battleships.round.ended")
+    public void roundEndedEvent(RoundEndedEvent event) throws IOException {
+        logger.info(String.format("#### -> Consumed message -> %s", event));
 
     }
 }
